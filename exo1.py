402a31ba8e2eb2b1a2619f457f762f8af568b756
Lourdine <lourdine.pascal@etud.polymtl.ca>
2026-05-20 13:40:00 -0400

    exo1 et exo2 completes

--- exo1.py
+++ exo1.py
@@ -22,12 +22,46 @@ Prompts EXACTS a utiliser :
 5) "Entrez la duree moyenne d'un spectacle live (en minutes) : "
 """
 
-# TODO: Lire le nom (str)
 
+try:
+    nom = input( "Entrez votre nom complet : ")
+    nbre_spectacle_electronique = int(input("Entrez le nombre de spectacles electroniques assistes au festival : "))
+    duree_spectacle_electronique =int(input( "Entrez la duree moyenne d'un spectacle electronique (en minutes) : "))
+    nbre_spectacle_live=int(input( "Entrez le nombre de spectacles live assistes au festival : "))
+    duree_spectacle_live=int(input("Entrez la duree moyenne d'un spectacle live (en minutes) : "))
+    
+    
+
+
+    if nbre_spectacle_electronique<0  or duree_spectacle_electronique <=0 or nbre_spectacle_live<0 or duree_spectacle_live<=0:
+        print("Erreur - donnees invalides.")
+    
+        
+# 
+    else:
+       
+        duree_total_el = duree_spectacle_electronique * nbre_spectacle_electronique
+        heure_electronique = duree_total_el // 60
+        min_electronique = duree_total_el% 60
+        duree_total_live= duree_spectacle_live * nbre_spectacle_live
+        heure_live = duree_total_live // 60
+        min_live = duree_total_live % 60
+        duree_total = duree_total_el + duree_total_live
+        heure_total = duree_total // 60
+        min_total = duree_total % 60
+
+        print(f"Bonjour {nom}")
+        print(f"Electronique: {nbre_spectacle_electronique} spectacle(s), {heure_electronique}h{min_electronique:02d} d'ecoute")
+        print(f"Live: {nbre_spectacle_live} spectacle(s), {heure_live}h{min_live:02d} d'ecoute")
+        print(f"Total: {heure_total}h{min_total:02d}")
+except:
+    print("Erreur - donnees invalides.")
+
+
+    
+# TODO: Lire le nom (str)
+#TODO: Calculer les minutes totales (electronique, live, total)
 # TODO: Lire les 4 valeurs (int)
 
 # TODO: Valider les donnees (spectacles >= 0, durees > 0)
-
-# TODO: Calculer les minutes totales (electronique, live, total)
-
 # TODO: Convertir en heures/minutes et afficher exactement 4 lignes
--- exo2.py
+++ exo2.py
@@ -18,10 +18,39 @@ Objectif :
     - derniere ligne : "     P Q R S T U V W"
 """
 
-FACTEURS = [1.20, 1.15, 1.05, 0.90, 0.90, 1.05, 1.15, 1.20]
+#FACTEURS = [1.20, 1.15, 1.05, 0.90, 0.90, 1.05, 1.15, 1.20]
 
 # TODO: Lire 8 entiers (un par ligne) dans une liste personnes
 #       En cas d'erreur de conversion ou valeur negative -> afficher le message d'erreur et quitter
+try:
+    
+    liste_de_personne = [int(input("Entrez une valeur:")) for valeur in range(8)]
+    if any(valeur < 0 for valeur in liste_de_personne):
+       print("Erreur - donnees invalides.") 
+    else:
+
+        liste_facteur =  [1.20, 1.15, 1.05, 0.90, 0.90, 1.05, 1.15, 1.20]  
+    
+        liste_intensite=[liste_de_personne[i] * liste_facteur[i] for i in range(8)]
+        if max(liste_intensite) == 0:
+            niveau_liste = [0 for i in range(8)]
+        else:
+            niveau_liste=[int((intensite/ max(liste_intensite)*10)+0.5) for intensite in liste_intensite]
+            
+        for i in range(10,0,-1):
+            
+            symboles = []
+
+            for j in range(8):
+                if niveau_liste[j] >= i: 
+                    symboles.append("❚")
+                else:
+                    symboles.append(".")
+            print(f"{i:2} | " + " ".join(symboles))
+            
+        print("     P Q R S T U V W")
+except:
+        print("Erreur - donnees invalides.") 
 
 # TODO: Calculer les intensites brutes (liste de 8 floats)
 
