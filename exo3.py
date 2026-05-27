43ce07c50282513e0d52fe6618255659f4d3b7df
Lourdine <lourdine.pascal@etud.polymtl.ca>
2026-05-27 10:41:47 -0400

    exo3 complet

--- exo3.py
+++ exo3.py
@@ -26,12 +26,52 @@ Prompts EXACTS :
 4) "Entrez le temps de controle a l'entree (en minutes) : "
 """
 
-# TODO: Importer math
+import math
+try:
+    distance = float(input("Entrez la distance jusqu'au Parc Jean-Drapeau (en kilometres) : "))
+    temps_velo = float(input("Entrez le temps d'attente pour un velo en libre-service (en minutes) : "))
+    temps_metro=float(input( "Entrez le temps du trajet en metro (en minutes) : "))
+    temps_controle = float(input( "Entrez le temps de controle a l'entree (en minutes) : "))
 
-# TODO: Lire les 4 valeurs
+    if distance  < 0 or temps_velo < 0 or temps_metro < 0 or  temps_controle < 0:
+        print("Erreur - donnees invalides.")
+    
+    
+    else:
+        marche = math.ceil(distance * 60 / 5 + temps_controle)
+        velo =  math.ceil(temps_velo + distance * 60 / 15 + temps_controle)
+        metro = math.ceil(temps_metro + temps_controle)
+
+        minimum=min(marche,velo,metro)
+
+        if  marche == velo== metro:
+               print("Egalite : marcher, velo et metro.")
+
+        elif marche == velo == minimum and minimum != metro:
+            print("Egalite : marcher et velo.")
+
+        elif metro == velo == minimum and minimum != marche:
+              print("Egalite : metro et velo.")
+
+        elif metro == marche == minimum and minimum != velo:
+            print("Egalite : marcher et metro.")
+
+        elif marche == minimum:
+            print(f"Option la plus rapide : marcher.")
+
+        elif velo == minimum:
+            print(f"Option la plus rapide : velo.")
+
+        elif metro == minimum:
+            print(f"Option la plus rapide : metro.")
+
+except:
+        print("Erreur - donnees invalides.")
+
+
+# TODO: Afficher la phrase exacte
+#TODO: Lire les 4 valeurs
 
 # TODO: Validation
 
 # TODO: Calculer, arrondir (ceil) et determiner le(s) meilleur(s)
-
-# TODO: Afficher la phrase exacte
